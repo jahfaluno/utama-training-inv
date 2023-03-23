c207e239899409a6bb5506d8192b6e2331e166ec
jahfaluno <118494075+jahfaluno@users.noreply.github.com>
2023-03-23 14:28:23 +0700

    commit inv

---
+++ appinvetarisbarang/app/views/partials/appfooter.php
@@ -0,0 +1,19 @@
+<footer class="footer border-top">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="copyright">[html-lang-0091] | &copy; <?php echo SITE_NAME ?> - <?php echo date('Y') ?></div>
+			</div>
+			<div class="col">
+				<div class="footer-links text-right">
+					<a href="<?php print_link('info/about'); ?>">[html-lang-0083]</a> | 
+					<a href="<?php print_link('info/help'); ?>">[html-lang-0087]</a> |
+					<a href="<?php print_link('info/contact'); ?>">[html-lang-0084]</a>  |
+					<a href="<?php print_link('info/privacy_policy'); ?>">[html-lang-0085]</a> |
+					<a href="<?php print_link('info/terms_and_conditions'); ?>">[html-lang-0086]</a>
+				</div>
+			</div>
+			__applanguagechange
+		</div>
+	</div>
+</footer>
